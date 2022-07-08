83f664d903d81c6d65faa63580a4aa7e3cda236a
Katya Vasylenko <kettyblack20@gmail.com>
2022-07-08 20:41:14 +0300

    taxi-service-kate.herokuapp.com to allowed hosts

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-b-2f#cn0n*e#rq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'taxi-service-kate.herokuapp.com']
 
 INTERNAL_IPS = [
     '127.0.0.1',
